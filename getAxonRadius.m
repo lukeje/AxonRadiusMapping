d73e73855f7abf7b5d02d1b494e3ddf93aaffd8f
Luke J. Edwards <ledwards@cbs.mpg.de>
2023-03-13 21:40:38 +0100

    Experimental 2nd order Neumann model

--- getAxonRadius.m
+++ getAxonRadius.m
@@ -1,26 +1,29 @@
 function [r, beta] = getAxonRadius(delta, Delta, g, y, model)
-
-    % "getAxonRadius": Estimate the axon radius from multi-shell diffusion
+    %getAxonRadius Estimate the axon radius from multi-shell diffusion
     % data, of which the lowest b-value is sufficiently high to suppress
     % the extra-axonal compartment. For in vivo human MRI, we recommend
     % a minimum b = 6000 s/mm2. 
     %
-    % [r, beta]  = getAxonRadius(delta, Delta, g, y, model)
-    %       output:
-    %           - r:    effective MR radius
-    %           - beta: the slope of the signal scaling as a function
-    %                   of 1/sqrt. ~ f/sqrt(Da)
-    
-    %       input:
-    %           - delta:  gradient duration [ms]
-    %           - Delta:  gradient separation [ms]
-    %           - g:      gradient strength [mT/m]
-    %           - y:      powder-averaged diffusion-weighted signal,
-    %                     normalized to y(b=0) = 1. If the dot compartment
-    %                     cannot be ignored, it need to be subtracted from 
-    %                     y prior to fitting.  
-    %           - model:  Van Gelderen or Neumann (the latter only applied 
-    %                     in long pulse regimes, but typically applies)
+    %     [r, beta] = getAxonRadius(delta, Delta, g, y, model)
+    %
+    % output:
+    %     - r:    effective MR radius
+    %     - beta: the slope of the signal scaling as a function
+    %             of 1/sqrt. ~ f/sqrt(Da)
+    %
+    % input:
+    %     - delta: gradient duration [ms]
+    %     - Delta: gradient separation [ms]
+    %     - g:     gradient strength [mT/m]
+    %     - y:     powder-averaged diffusion-weighted signal,
+    %              normalized to y(b=0) = 1. If the dot compartment
+    %              cannot be ignored, it needs to be subtracted from 
+    %              y prior to fitting.  
+    %     - model: "Van Gelderen", "Neumann" (the latter only applies 
+    %              in long pulse regimes, but typically applies), or
+    %              "Neumann2ndOrder" (second order cumulant expansion
+    %              of the Neumann model valid for stronger gradient
+    %              strengths)
     %
     %  Author: Jelle Veraart (jelle.veraart@nyulangone.org)
     %  Copyright (c) 2019 New York University
@@ -34,14 +37,19 @@ function [r, beta] = getAxonRadius(delta, Delta, g, y, model)
 
     gyroMagnRatio = 267.513e-6;
     q = g*gyroMagnRatio;
- 
-    % Random initial guess seems to work well for Neumann, but not for Van
-    % Gelderen model, so we use the Neumann results as initial guesses for
-    % the Van Gelderen model
+
     start = [sqrt(4*pi)*rand(), 1.5+2*rand()];
-    pars = lsqnonlin(@(x)residuals(x, [delta(:), Delta(:), g(:)], y, 'Neumann'),start,[0 0],[sqrt(4*pi) 5],options);
-    if strcmp(model,'VanGelderen')
-        pars = lsqnonlin(@(x)residuals(x, [delta(:), Delta(:), g(:)], y, 'VanGelderen'),pars,[0 0],[sqrt(4*pi) 5],options);
+    switch model
+        case {'Neumann','Neumann2ndOrder'}
+            pars = lsqnonlin(@(x)residuals(x, [delta(:), Delta(:), g(:)], y, model),start,[0 0],[sqrt(4*pi) 5],options);
+        case 'VanGelderen'
+            % Random initial guess seems to work well for Neumann, but not for Van
+            % Gelderen model, so we use the Neumann results as initial guesses for
+            % the Van Gelderen model
+            pars = lsqnonlin(@(x)residuals(x, [delta(:), Delta(:), g(:)], y, 'Neumann'),start,[0 0],[sqrt(4*pi) 5],options);
+            pars = lsqnonlin(@(x)residuals(x, [delta(:), Delta(:), g(:)], y, 'VanGelderen'),pars,[0 0],[sqrt(4*pi) 5],options);
+        otherwise
+            error('Unknown model "%s". Allowed models are "Neumann", "Neumann2ndOrder", or "VanGelderen"', model)
     end
     beta = pars(1);
     r = pars(2);
@@ -71,18 +79,23 @@ function [s, ds] = AxonDiameterFWD(delta, Delta, g, pars, model)
     q = g*gyroMagnRatio;
     b = (q.*delta).^2.*(Delta - delta/3);
  
-
     % Forward model
     switch model
         case 'Neumann'
-            s = beta * exp(-(7/48)*q.^2.*delta*r^4/D0) ./ sqrt(b);
             ds_dbeta = exp(-(7/48)*q.^2.*delta*r^4/D0) ./ sqrt(b);
+            s = beta * ds_dbeta;
             ds_dr = s .* -(7/12)*q.^2.*delta*r^3/D0;
+
+        case 'Neumann2ndOrder'
+            logs1 = -(7/48)*q.^2.*delta*r^4/D0;
+            ds_dbeta = exp(logs1 -0.5*logs1.^2) ./ sqrt(b);
+            s = beta * ds_dbeta;
+            ds_dr = s .* -(7/12)*q.^2.*delta*r^3.*(1 - logs1)/D0;
 
         case 'VanGelderen'
             [Svg, dSvg] = vg(delta, Delta, q, r, D0);
-            s = beta * exp(-Svg) ./ sqrt(b);
             ds_dbeta = exp(-Svg) ./ sqrt(b);
+            s = beta * ds_dbeta;
             ds_dr = s .* -dSvg;
     end
 
